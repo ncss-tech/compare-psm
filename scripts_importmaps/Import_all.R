3e43b4877fc5ddd66199466b4011d7860f33b522
Rossiter, David G <david.rossiter@isric.org>
2020-11-26 11:53:19 -0500

    small updates

--- scripts_importmaps/Import_all.R
+++ scripts_importmaps/Import_all.R
@@ -5,7 +5,7 @@
 ## voi
 ## depth
 
-import.all <- function(aoi, voi, depth, quantile, which=1:6) {
+import.all <- function(aoi, voi, depth, size, quantile, which=1:6) {
   library(knitr)
   psm.list <- c(
     "GSM_USA_V05",
@@ -19,6 +19,7 @@ import.all <- function(aoi, voi, depth, quantile, which=1:6) {
                       params = list(aoi=aoi,
                                     voi=voi,
                                     depth=depth,
+                                    size=size,
                                     quantile=quantile),
                       output_file = paste0(aoi, '_', voi, '_', depth, '.html'),
                       envir = parent.frame()
