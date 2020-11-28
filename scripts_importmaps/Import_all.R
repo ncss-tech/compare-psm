a1ffe1c109f0b7ec24335878df582fe0cf7988ed
Rossiter, David G <david.rossiter@isric.org>
2020-11-28 15:09:05 -0500

    parameterize all scripts, fix import-all script

--- scripts_importmaps/Import_all.R
+++ scripts_importmaps/Import_all.R
@@ -1,11 +1,16 @@
 # Function to import all with parameters, 
 #  by rendering their respective R Markdown scripts
 # Arguments:
-## aoi
-## voi
-## depth
+# lrc_long: -76
+# lrc_lat: 42 
+# size: 1
+# voi.n: 4
+# depth.n: 4
+# quantile.n: 4 
 
-import.all <- function(aoi, voi, depth, size, quantile, which=1:6) {
+## see lists of VOI, depth, quantile in `../ComparePSMproductsUSA.Rmd`
+
+import.all <- function(lrc_long, lrc_lat, size=1, voi.n, depth.n=1, quantile.n=4, which=1:6) {
   library(knitr)
   psm.list <- c(
     "GSM_USA_V05",
@@ -14,16 +19,25 @@ import.all <- function(aoi, voi, depth, size, quantile, which=1:6) {
     "SPCG100USA",
     "LandGIS",
     "ISSR8")
-  for (psm in psm.list) {
-    rmarkdown::render(psm,
-                      params = list(aoi=aoi,
-                                    voi=voi,
-                                    depth=depth,
-                                    size=size,
-                                    quantile=quantile),
-                      output_file = paste0(aoi, '_', voi, '_', depth, '.html'),
-                      envir = parent.frame()
+  for (psm in psm.list[which]) {
+    print(paste("Importing", psm))
+    param.list <- list(lrc_long=lrc_long,
+                       lrc_lat=lrc_lat,
+                       size=size,
+                       voi.n=voi.n,
+                       depth.n=depth.n,
+                       quantile.n=quantile.n)
+    
+    out.file.name <- paste0(lrc_long, '_', lrc_lat, '_', 
+                             voi.n, '_', quantile.n, '_', 
+                             depth.n, '.html')
+    
+    rmarkdown::render(paste0(psm, "_import.Rmd"),
+                      params = param.list,
+                      output_file = out.file.name,
+                      envir = new.env(parent = globalenv())
                       )
   }
 }
 
+import.all(lrc_long=-91, lrc_lat=36, size=1, voi.n=1, depth.n=4, quantile.n=4, which=c(3))
