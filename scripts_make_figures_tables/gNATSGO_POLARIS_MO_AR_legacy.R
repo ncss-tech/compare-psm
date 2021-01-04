c48fa1e7d27c30ebbd07ae996460cc6c35e33ed7
Rossiter, David G <david.rossiter@isric.org>
2021-01-04 12:50:53 -0800

    "final" compare patterns script

--- scripts_make_figures_tables/gNATSGO_POLARIS_MO_AR_legacy.R
+++ scripts_make_figures_tables/gNATSGO_POLARIS_MO_AR_legacy.R
@@ -1,13 +1,13 @@
 # figure to illustrate inconsistency in legacy gSSURGO
 
 library(terra)
-# GSM tile imported by GSM_USA_V05_import.Rmd
-(r.gsm <- rast("/Users/rossiter/ds/GSM_USA/lat3637_lon-92-91/GSM_mu_claytotal_r_g_kg_030_060.tif"))
-r.gsm <- r.gsm/10 # convert back to %clay
-ext(r.gsm)
-pdf("/Users/rossiter/ds/GSM_USA/lat3637_lon-92-91/MO_AR_GSM_clay_pct_030_060.pdf",
+# gNATSGO tile imported by gNATSGO_USA_V05_import.Rmd
+(r.gnatsgo <- rast("/Users/rossiter/ds/gNATSGO/lat3637_lon-92-91/claytotal_r_3060.tif"))
+# already %clay
+ext(r.gnatsgo)
+pdf("/Users/rossiter/ds/gNATSGO/lat3637_lon-92-91/MO_AR_gNATSGO_clay_pct_030_060.pdf",
     width=5.5, height=6.5)
-terra::plot(r.gsm, col=topo.colors(64), range=c(0,100))
+terra::plot(r.gnatsgo, col=topo.colors(64), range=c(0,100))
 dev.off()
 
 # POLARIS tile imported by POLARIS_import.Rmd
