051dca5286301a08fb6c245800df844605845ff6
Rossiter, David G <david.rossiter@isric.org>
2021-03-18 15:03:17 -0400

    compare_all_local

--- scripts_compare/Compare_all_local.R
+++ scripts_compare/Compare_all_local.R
@@ -7,42 +7,43 @@
 # voi.n: 4
 # depth.n: 4
 # quantile.n: 4 
+# test.tile.size: 0.15  # degrees
+# test.tile.x.offset: 0.6  # west from right edge
+# test.tile.y.offset: 0.2  # north from bottom edge
+## see lists of VOI, depth, quantile in `Compare_local.Rmd`
 
-## see lists of VOI, depth, quantile in `Compare_regional.Rmd`
-
-## the list of products to compare must be set within `Compare_regional.Rmd`
-##  and `Compare_regional_patterns.Rmd`
-## also the subtile limits for patterns
+## the products to compare are hard-coded in `Compare_local.Rmd`
+##  and `Compare_local_patterns.Rmd`
 
 compare.one <- function(lrc_long, lrc_lat, voi.n, depth.n, test.tile.size, test.tile.x.offset, test.tile.y.offset) {
   library(knitr)
-  param.list <- list(lrc_long=lrc_long,
+  param.list.patterns <- list(lrc_long=lrc_long,
                      lrc_lat=lrc_lat,
                      size=1,
                      voi.n=voi.n,
                      depth.n=depth.n,
-                     quantile.n=4)  # only compare means
+                     quantile.n=4) 
+  # only compare means
+  param.list <- c(param.list.patterns,  
+                     test.tile.size=test.tile.size,
+                     test.tile.x.offset=test.tile.x.offset,
+                     test.tile.y.offset=test.tile.y.offset)
   
-  param.list.patterns <- c(param.list, # for the pattern analysis
-                           test.tile.size=test.tile.size,
-                           test.tile.x.offset=test.tile.x.offset,
-                           test.tile.y.offset=test.tile.y.offset)
-  
-  out.file.name <- paste0("Compare_", lrc_long, '_', lrc_lat, '_', 
+  out.file.name <- paste0("CompareLocal", lrc_long, '_', lrc_lat, '_', 
                           voi.n, '_', 
                           depth.n, '.html')
   
-  rmarkdown::render("Compare_regional.Rmd",
+  rmarkdown::render("Compare_local.Rmd",
                     params = param.list,
                     output_file = out.file.name,
                     envir = new.env(parent = globalenv())
   )
   
-  out.file.name <- paste0("ComparePattern_", lrc_long, '_', lrc_lat, '_', 
+  out.file.name <- paste0("CompareLocalPattern_", lrc_long, '_', lrc_lat, '_', 
                           voi.n, '_', 
                           depth.n, '.html')
   
-  rmarkdown::render("Compare_regional_patterns.Rmd",
+  rmarkdown::render("Compare_local_patterns.Rmd",
                     params = param.list.patterns,
                     output_file = out.file.name,
                     envir = new.env(parent = globalenv())
@@ -56,8 +57,8 @@ for (d.n in c(1,4)) {
   compare.one(lrc_long=-76, lrc_lat=42, 
               voi.n=4,  # voi.list.sg <- c("clay", "silt", "sand", "phh2o", "cec", "soc", "bdod", "cfvo")
               depth.n=d.n, 
-              test.tile.size=0.2,
-              test.tile.x.offset=0.6,
-              test.tile.y.offset=0.2
+              test.tile.size=0.15,
+              test.tile.x.offset=0.3,
+              test.tile.y.offset=0.45
   )
 }
